419cd19dd4237dcc569b1e31b2bbeaa30928e4c0
Duwe Aynab <mfaynab88@gmail.com>
2022-06-07 20:47:05 -0400

    Update User.java

--- src/main/java/com/tts/TechTalentTwitter/model/User.java
+++ src/main/java/com/tts/TechTalentTwitter/model/User.java
@@ -5,8 +5,12 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.validator.constraints.Length;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
 import java.util.Date;
 import java.util.Set;
 
@@ -21,9 +25,15 @@ public class User {
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "user_id")
     private Long id;
-
+    @Email(message= "Please provide a valid email")
+    @NotEmpty(message = "Please provide an email")
     private String email;
+
+    @Length(min = 3, message = "Your username must hae at least 3 characters")
+    @Length(max = 15, message = "Your username cannot have more than 15 characters")
+    @Pattern(regexp = "[^\\s]+", message = "Your username cannot contain spaces")
     private String username;
+    @Length(min = 5, message = "Your password must at least have 5 characters")
     private String password;
     private String firstName;
     private String lastName;
